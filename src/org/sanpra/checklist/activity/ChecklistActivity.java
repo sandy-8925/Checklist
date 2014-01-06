e8961fb8e6c5acba5b9f7de981f090e7f52f7050
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-05 22:15:11 -0800

    Remove unused import

--- src/org/sanpra/checklist/activity/ChecklistActivity.java
+++ src/org/sanpra/checklist/activity/ChecklistActivity.java
@@ -27,7 +27,6 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
