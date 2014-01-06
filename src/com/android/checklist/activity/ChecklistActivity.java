sandy-8925/Checklist

commit 522e69a2e6563de4e6ce93a1024d1d27d05da454

Specify checklist item colour values in values/colour_values.xml file and update code to read values from that file

src/com/android/checklist/activity/ChecklistActivity.java
@@ -49,8 +49,8 @@ public class ChecklistActivity extends ListActivity {
 	class ChecklistItemAdapter extends SimpleCursorAdapter {
 		// TODO: Check if these colours are better specified in an Android
 		// resource
-		private static final int CHECKLIST_ITEM_UNCHECKED_COLOUR = 0xFFFFFFFF;
-		private static final int CHECKLIST_ITEM_CHECKED_COLOUR = 0x88888888;
+		private static final int CHECKLIST_ITEM_UNCHECKED_COLOUR = R.integer.white;
+		private static final int CHECKLIST_ITEM_CHECKED_COLOUR = R.integer.grey;
 
 		ChecklistItemAdapter() {
 			super(ChecklistActivity.this, R.layout.item_row, mItemsCursor,

res/values/colour_values.xml
@@ -1,0 +1,5 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+	<integer name="grey">2290649224</integer>
+	<integer name="white">4294967295</integer>
+</resources>

commit 2b3c8fd800e3a6f7b53adc831d530c1c1d986b98

Fix bug introduced in 522e69a - assigned integer IDs to the colour variables by mistake, instead of fetching the resources associated with the IDs

src/com/android/checklist/activity/ChecklistActivity.java
@@ -19,6 +19,7 @@ package com.android.checklist.activity;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -49,12 +50,15 @@ public class ChecklistActivity extends ListActivity {
 	class ChecklistItemAdapter extends SimpleCursorAdapter {
 		// TODO: Try to use Android colour resources to specify the colour instead of setting
 		// the integer colour value
-		private static final int CHECKLIST_ITEM_UNCHECKED_COLOUR = R.integer.white;
-		private static final int CHECKLIST_ITEM_CHECKED_COLOUR = R.integer.grey;
+		private int CHECKLIST_ITEM_UNCHECKED_COLOUR;
+		private int CHECKLIST_ITEM_CHECKED_COLOUR;
 
 		ChecklistItemAdapter() {
 			super(ChecklistActivity.this, R.layout.item_row, mItemsCursor,
 					from, to);
+			Resources appResources = getResources();
+			CHECKLIST_ITEM_UNCHECKED_COLOUR = appResources.getInteger(R.integer.white);
+			CHECKLIST_ITEM_CHECKED_COLOUR = appResources.getInteger(R.integer.grey);
 		}
 
 		public View getView(int position, View convertView, ViewGroup parent) {