7ddac868c043261f9b090ffb59cd6c1f8c2ded48
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-09-28 21:50:48 -0700

    ChecklistActivity.onCreate: Get reference to main ListView of the activity

--- src/org/sanpra/checklist/activity/ChecklistActivity.java
+++ src/org/sanpra/checklist/activity/ChecklistActivity.java
@@ -95,6 +95,7 @@ public class ChecklistActivity extends ListActivity {
         from = new String[] { ItemsDbHelper.COL_DESC };
         to = new int[] { R.id.itemtext };
         itemListAdapter = new ChecklistItemAdapter();
+        ListView itemsListView = (ListView) findViewById(android.R.id.list);
         setListAdapter(itemListAdapter);
 
         registerForContextMenu(getListView());
