465ba02d59af569709e118bbb701f98d7c393a74
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-11-19 17:20:32 -0800

    ChecklistItemsCursorLoader: Reformat code

--- src/org/sanpra/checklist/dbhelper/ChecklistItemsCursorLoader.java
+++ src/org/sanpra/checklist/dbhelper/ChecklistItemsCursorLoader.java
@@ -7,15 +7,15 @@ import com.commonsware.cwac.loaderex.acl.AbstractCursorLoader;
 
 public final class ChecklistItemsCursorLoader extends AbstractCursorLoader {
 
-	private final Context mContext;
+    private final Context mContext;
 
-	public ChecklistItemsCursorLoader(final Context context) {
-		super(context);
-		mContext = context;
-	}
+    public ChecklistItemsCursorLoader(final Context context) {
+        super(context);
+        mContext = context;
+    }
 
-	@Override
-	protected Cursor buildCursor() {
-		return 	ItemsDbHelper.getInstance(mContext).fetchAllItems();
-	}
+    @Override
+    protected Cursor buildCursor() {
+        return ItemsDbHelper.getInstance(mContext).fetchAllItems();
+    }
 }
