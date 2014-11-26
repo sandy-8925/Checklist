d56a1d51ce34b58a145fd8f80b3073a42328af71
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-11-25 16:14:21 -0800

    Add subdirectory test, that contains Android test project, which contains tests for this project

---
+++ test/src/org/sanpra/checklist/test/ChecklistActivityTest.java
@@ -0,0 +1,12 @@
+package org.sanpra.checklist.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import org.sanpra.checklist.activity.ChecklistActivity;
+
+public class ChecklistActivityTest extends ActivityInstrumentationTestCase2<ChecklistActivity> {
+
+    public ChecklistActivityTest() {
+        super(ChecklistActivity.class);
+    }
+
+}
